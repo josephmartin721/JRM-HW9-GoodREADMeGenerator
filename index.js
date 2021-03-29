4d5fd7c5e5a0c7d01b37271877d1279deec0cbe0
Joseph Martin <josephmartin721@yahoo.com>
2021-03-29 04:21:18 -0500

    placeholder code

--- index.js
+++ index.js
@@ -5,7 +5,47 @@ const util = require('util');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+  type: "input",
+  message: "Enter your Github username: ",
+  name: "username"
+},
+{
+  type: "input",
+  message: "Enter your project title: ",
+  name: "title"
+},
+{
+  type: "input",
+  message: "Enter a short project description: ",
+  name: "description"
+},
+{
+  type: "input",
+  message: "Enter the installation process: ",
+  name: "installation"
+},
+{
+  type: "input",
+  message: "Enter the usage of the project: ",
+  name: "usage"
+
+},
+{
+  type: "input",
+  message: "Enter licenses used for the project: ",
+  name: "licenses"
+},
+{
+  type: "input",
+  message: "Enter contributing information: ",
+  name: "contribute"
+
+}, {
+  type: "input",
+  message: "Enter any tests you are running for your project: ",
+  name: "tests"
+}];
 
 
 // TODO: Create a function to write README file
